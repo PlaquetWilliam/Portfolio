bec98a05fa8201eeefdcadd6f338537ed5ad834e
William Plaquet <william.plaquet@bts-malraux.net>
2024-10-10 12:30:22 +0200

    Changement portolio

--- src/pages/Theme1.tsx
+++ src/pages/Theme1.tsx
@@ -14,9 +14,9 @@ export const Theme1 = () => {
             <div className='flex flex-col justify-center items-center w-1/2'>
                 <h1 className="text-6xl font-bold">Développement de site internet</h1>
                 <p className="mt-4 text-xl font-medium text-center">
-                    Pendant mon temps libre, j'aime créer divers sites pour des besoins professionnels ainsi que personnels. Voici ci-dessous 
-                    quelques exemples de projets personnels que j'ai réalisés durant mon apprentissage dans le domaine du développement web (Ayant 
-                    des problèmes concernant l'hébergement ces projets fonctionne seulement en local donc voici simplement des screnshots).
+                    Pendant mon temps libre, j'aime créer divers sites pour des besoins professionnels ainsi que personnels. Voici ci-dessous quelques exemples 
+                    de projets personnels que j'ai réalisés durant mon apprentissage dans le domaine du développement web. (Ayant des problèmes concernant l'hébergement, 
+                    ces projets fonctionnent seulement en local donc voici simplement des screnshots).
                 </p>
                 <div className='flex mt-6'>
                     <Link to='/theme2' className="py-2 px-6 font-medium text-white bg-blue-500 rounded-lg transition duration-150 ease-out hover:bg-blue-600">Voir intélligence artificielle</Link>
